200cb517d58a38f8a86f536ff7b288a1ff9fdd4d
unknown <edu230991@gmail.com>
2024-03-19 17:28:22 +0100

    appending string to tag

--- .github/workflows/append_env_var.py
+++ .github/workflows/append_env_var.py
@@ -2,9 +2,11 @@
 """
 
 import os
+import datetime as dt
 from pyenerginet import __version__
 
 env_file = os.getenv("GITHUB_ENV")
+now_str = dt.datetime.now(dt.UTC).strftime("%Y%m%d%H%M%S")
 
 with open(env_file, "a") as myfile:
-    myfile.write(f"VERSION_TAG=v{__version__}")
+    myfile.write(f"VERSION_TAG=v{__version__}-{now_str}")
